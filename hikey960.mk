07d98db1bf575927e0c74955aa79e44a99cfe4b2
Sergey_K <sergeykishko@gmail.com>
2020-06-08 15:31:29 +0300

    Modified hikey960.mk to use patched kernel
    Modification for use patch kernel
    
    Signed-off-by: Sergey_K <sergeykishko@gmail.com>

--- hikey960.mk
+++ hikey960.mk
@@ -1,8 +1,12 @@
 ifndef TARGET_KERNEL_USE
 TARGET_KERNEL_USE=4.9
+else
+$(error only default 4.9 is supported)
 endif
-TARGET_PREBUILT_KERNEL := device/linaro/hikey-kernel/Image.gz-hikey960-$(TARGET_KERNEL_USE)
-TARGET_PREBUILT_DTB := device/linaro/hikey-kernel/hi3660-hikey960.dtb-$(TARGET_KERNEL_USE)
+
+PRODUCT_OUT_DIR := out/target/product/hikey960
+TARGET_PREBUILT_KERNEL := $(PRODUCT_OUT_DIR)/Image.gz-hikey960-$(TARGET_KERNEL_USE)
+TARGET_PREBUILT_DTB := $(PRODUCT_OUT_DIR)/hi3660-hikey960.dtb-$(TARGET_KERNEL_USE)
 
 ifeq ($(TARGET_KERNEL_USE), 4.4)
   HIKEY_USE_LEGACY_TI_BLUETOOTH := true
@@ -16,7 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/linaro/hikey/hikey960/device-hikey960.mk)
 $(call inherit-product, device/linaro/hikey/device-common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
 #
 # Overrides
 PRODUCT_NAME := hikey960
